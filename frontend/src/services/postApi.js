1c6427d8f20ff8466566538e3b1dc5f04d3862a8
Thanaphoom Babparn <mart-tp@hotmail.com>
2018-11-15 09:11:51 +0700

    Fix path localhost

--- frontend/src/services/postApi.js
+++ frontend/src/services/postApi.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 export const fetchAllPost = () => {
   return new Promise((resolve, reject) => {
     axios
-      .get(`http://localhost:5000/social-center/posts/`)
+      .get(`/social-center/posts/`)
+      // .get(`http://localhost:5000/social-center/posts/`)
       .then(result => {
         console.log(result.data)
         resolve(result.data.posts)
@@ -17,7 +18,8 @@ export const fetchAllPost = () => {
 export const fetchPostByName = name => {
   return new Promise((resolve, reject) => {
     axios
-      .get(`http://localhost:5000/social-center/posts/${name}`)
+      .get(`/social-center/posts/${name}`)
+      // .get(`http://localhost:5000/social-center/posts/${name}`)
       .then(result => {
         console.log(result.data)
         resolve(result.data.posts)
@@ -31,7 +33,8 @@ export const fetchPostByName = name => {
 export const addNewPost = post => {
   return new Promise((resolve, reject) => {
     axios
-      .post(`http://localhost:5000/social-center/posts/`, post)
+      .post(`/social-center/posts/`, post)
+      // .post(`http://localhost:5000/social-center/posts/`, post)
       .then(result => {
         console.log(result.data)
         resolve(result.data.newPost)
@@ -45,7 +48,8 @@ export const addNewPost = post => {
 export const deletePostById = id => {
   return new Promise((resolve, reject) => {
     axios
-      .delete(`http://localhost:5000/social-center/posts/${id}`)
+      .delete(`/social-center/posts/${id}`)
+      // .delete(`http://localhost:5000/social-center/posts/${id}`)
       .then(result => {
         console.log(result.data)
         resolve(result.data.deletedPosts)
